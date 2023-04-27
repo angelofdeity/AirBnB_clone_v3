7bda835ab68011bc6d03c17ccba06450ef3803ef
efezino idisi <enduranceidisi13@gmail.com>
2023-04-27 23:57:10 +0100

    add stats route and 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 This module contains a simple flask application
 """
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,7 +11,13 @@ from os import getenv
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    """handle 404 errors"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,31 @@ This module contains the routes for the web application
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models import storage
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route("/status")
 def status():
     """status of API"""
     return jsonify({'status': "OK"})
+
+
+@app_views.route("/stats")
+def number_of_objects():
+    """retrieves the number of each objects by type"""
+    objs = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+    }
+
+    return jsonify(objs)
